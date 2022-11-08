d4b5b00f7eb6d522fb31c9f16a080772f1e32225
alicelee1231 <saemmilee1231@gmail.com>
2022-11-08 23:30:07 +0900

    별 트리 생

---
+++ study/src/ch05/Tree.java
@@ -0,0 +1,17 @@
+package ch05;
+
+public class Tree {
+
+	public static void main(String[] args) {
+		
+		for( int i =0; i < 5; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	
+
+	}
+
+}
